7033b7b55310ab4c49ce0dc309ed2702d1a723ac
Anna Baker <62150513+abaker93@users.noreply.github.com>
2020-06-03 09:50:31 -0400

    Lesson 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
-import App from "./App";
+function TicTacToe() {
+	return (
+		<div className="grid">
+			<Square position={0} />
+			<Square position={1} />
+			<Square position={2} />
+			<Square position={3} />
+			<Square position={4} />
+			<Square position={5} />
+			<Square position={6} />
+			<Square position={7} />
+			<Square position={8} />
+		</div>
+	);
+}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+function Square() {
+	return <div className="square">Square</div>;
+}
+
+function Circle() {
+	return <div>Circle</div>;
+}
+
+function Cross() {
+	return <div>Cross</div>;
+}
+
+function Result() {
+	return <div>Result</div>;
+}
+
+ReactDOM.render(<TicTacToe />, document.getElementById("app"));
