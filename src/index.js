b8b27b67dd396e9b714ba2008de23b9969683edd
Anna Baker <62150513+abaker93@users.noreply.github.com>
2020-06-03 12:17:06 -0400

    Lesson 12

--- src/index.js
+++ src/index.js
@@ -58,55 +58,75 @@ function TicTacToe() {
 		});
 	}
 
+	function reset() {
+		setState({
+			player: CIRCLE,
+			positions: [
+				EMPTY,
+				EMPTY,
+				EMPTY,
+				EMPTY,
+				EMPTY,
+				EMPTY,
+				EMPTY,
+				EMPTY,
+				EMPTY
+			]
+		});
+	}
+
 	const winner = detectWinner(state.positions);
 
 	return (
-		<div className="grid">
-			<Square
-				position={0}
-				value={state.positions[0]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={1}
-				value={state.positions[1]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={2}
-				value={state.positions[2]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={3}
-				value={state.positions[3]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={4}
-				value={state.positions[4]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={5}
-				value={state.positions[5]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={6}
-				value={state.positions[6]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={7}
-				value={state.positions[7]}
-				takeTurn={takeTurn}
-			/>
-			<Square
-				position={8}
-				value={state.positions[8]}
-				takeTurn={takeTurn}
-			/>
+		<div>
+			<div className="grid">
+				<Square
+					position={0}
+					value={state.positions[0]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={1}
+					value={state.positions[1]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={2}
+					value={state.positions[2]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={3}
+					value={state.positions[3]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={4}
+					value={state.positions[4]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={5}
+					value={state.positions[5]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={6}
+					value={state.positions[6]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={7}
+					value={state.positions[7]}
+					takeTurn={takeTurn}
+				/>
+				<Square
+					position={8}
+					value={state.positions[8]}
+					takeTurn={takeTurn}
+				/>
+			</div>
+			{winner && <Result winner={winner} reset={reset} />}
 		</div>
 	);
 }
@@ -155,8 +175,15 @@ function Cross() {
 	);
 }
 
-function Result() {
-	return <div>Result</div>;
+function Result({ winner, reset }) {
+	return (
+		<div className="result">
+			{winner === CIRCLE && 'Circle won the game'}
+			{winner === CROSS && 'Cross won the game'}
+			{winner === 'It is a tie' && 'It is a tie'}
+			<button onClick={reset}>Reset</button>
+		</div>
+	);
 }
 
 ReactDOM.render(<TicTacToe />, document.getElementById('app'));
